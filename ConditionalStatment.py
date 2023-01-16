ecf3183d212ca45c12d89f9127fce5e5b7bcdd1c
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2023-01-16 18:49:02 +0530

    if-else

---
+++ ConditionalStatment.py
@@ -0,0 +1,144 @@
+'''
+1. if else
+2. switch  -> not available in python
+3. loop
+
+
+-> simple if
+-> if else
+-> nested if
+-> else if ladder
+
+
+'''
+'''
+-> In C Programming
+age=?
+if(age>=18)
+{
+    printf("Eligible");
+}else{
+    printf("Not Eligible");
+}
+
+'''
+
+# age = int(input("Enter Your age:  "))
+
+# if (age>=18):
+#     print("Eligible for Vote")
+# else:
+#     print("Not Eligible for Vote")
+
+# print('Thank You')
+
+"""
+
+"""
+
+# n=5
+
+# if n>30:
+#     print("hello")
+# else:
+#    print()
+# print("Thank YOU!!")
+
+'''
+Take one number from user and check number number is positive or negative.
+
+'''
+
+# n=int(input("Enter a no: "))
+
+# if n>0:
+#     if n>10:
+#         print(n , "is greater than 10")
+#     else:
+#         print(n , "is less than 10")
+# elif n==0:
+#     print("ZERO")
+# else:
+#     print(n,"is Negative")
+
+
+# if(n%3==0 and n%11==0):
+#     print("Divisible")
+# else:
+#     print("Not divisible")
+
+'''
+1. Take one number from user and check number is odd or even.
+2. Take two number from user and find minimum number. 
+3. Take three number from user and find maximum number.
+4. Take one number from user and check number is divisible by 3 and 11 or not ?
+
+    33 - divisible
+    6 - not divisible
+
+5. Take cost price and selling price from user and find profit or loss.
+
+    cp - 200
+    sp - 500
+    profit - 300
+
+
+    cp-100
+    sp-50
+    loss-50
+
+6. Write a program to accept the cost price of bike display the road tax to be paid 
+    according to the following criteria:
+
+    cost price (in Rs)       Tax
+    >100000                  15%
+    >50000and <=100000       10%
+    <=50000                   5% 
+
+7. Take one number from user and print week days according to that number.
+    1 - Sunday
+    2 - Monday
+    .
+    .
+    7 - Satuarday
+    
+    other input: Invalid 
+
+8.  Take rupeess from user and count total number of notes.
+
+n - 1678
+
+500 - 3
+200 - 0
+100 - 1
+50 - 1
+10 - 2
+5 - 1
+1 - 3
+
+
+n - 2000
+
+500- 4
+200 - 0
+100 - 0
+50 - 0
+10 - 0
+5 - 0
+1 - 0
+
+
+
+
+
+'''
+
+a = int(input("ENter a no: "))
+
+# if a%2==0: print("Even Number");print("Hello...")
+# else: print("Odd Number")
+
+print("Even Number") if a%2==0 else print("Odd Number")
+
+
+
